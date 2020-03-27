b8c3b4232c39a7a77e7916b473b65b913990f562
shakil ahmed <shakil.austcse27@gmail.com>
2020-03-27 11:12:32 +0600

    Create 280. Wiggle Sort.cpp

---
+++ 280. Wiggle Sort.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        if( nums.empty() ) return ;
+        for( int i = 0 ; i < nums.size() - 1 ; i++ ) { 
+            if( i % 2 == 0 ) { 
+                if( nums[i] > nums[i+1] ) swap( nums[i], nums[i+1] );
+            } else { 
+                if( nums[i+1] > nums[i] ) swap(nums[i], nums[i+1]);
+            }
+        }
+    }
+};
