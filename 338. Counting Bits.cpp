bf460247b1f6708afda183a6c2d4151558a303dd
shakil ahmed <shakil.austcse27@gmail.com>
2020-04-20 17:08:10 +0600

    Create 338. Counting Bits.cpp

---
+++ 338. Counting Bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector < int > dp( num + 1 , 0 );
+        for( int i = 1; i <= num ; i++ ) { 
+            dp[i] = dp[i/2] + i % 2;
+        }
+        return dp;
+    }
+};
