c89c2de2302106247c6f1747cd2be370ade99956
shakil ahmed <shakil.austcse27@gmail.com>
2020-08-05 13:45:29 +0600

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       int freq[26] = {0};
+       for( char ch: s ) { 
+            freq[ch - 'a']++;
+       }
+        
+       for(char ch: t) { 
+            freq[ch - 'a']--;
+        }
+        
+        for( int i = 0 ; i < 26 ; i++ ) { 
+            if( freq[i] != 0 ) return false;
+        }
+        
+        return true;
+    }
+};
