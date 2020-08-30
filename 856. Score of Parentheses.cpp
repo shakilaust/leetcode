86995d902d8c53695dd20403fed75f56758c7479
shakil ahmed <shakil.austcse27@gmail.com>
2020-08-30 18:22:11 +0600

    Create 856. Score of Parentheses.cpp

---
+++ 856. Score of Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        int ans = 0;
+        int bal = 0;
+        for( int i = 0 ; i < S.size() ; i++ ) { 
+            if( S[i] == '(') bal++;
+            else {
+                bal--;
+                if( S[i-1] == '(' ) { 
+                    ans += ( 1 << bal );
+                }
+            }
+        }
+        return ans;
+    }
+};
