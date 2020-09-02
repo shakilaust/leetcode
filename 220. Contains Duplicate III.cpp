777564576bf14257de95c38928d09f6b4db6dd6b
shakil ahmed <shakil.austcse27@gmail.com>
2020-09-02 18:25:04 +0600

    Create 220. Contains Duplicate III.cpp

---
+++ 220. Contains Duplicate III.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        set < long > window;
+        for( int i = 0 ; i < nums.size() ; i++ ) { 
+            
+            if( i > k ) window.erase(nums[i-k-1]);
+            auto pos = window.lower_bound((long long int) nums[i] - t);
+            
+            if( pos != window.end() && ( *pos - nums[i]) <= t ) { 
+                return true;
+            }
+            
+            window.insert( nums[i]);
+
+        }
+        return false;
+    }
+};
