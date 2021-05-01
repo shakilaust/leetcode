304fb34ea567cd451a54fc128c42d610d7b74519
shakil ahmed <shakil.austcse27@gmail.com>
2021-05-02 02:07:19 +0600

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size();
+        while( low < high ) {
+            int mid = low + ( high - low ) / 2;
+            if( nums[mid] < target ) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
